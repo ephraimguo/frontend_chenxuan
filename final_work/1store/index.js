e392bc3b665c313cd998bcd123bf10b08d98fd61
ephraimguo <ephraimguo@gmail.com>
2017-12-21 11:23:45 +0800

    chenxuan csdn front end tut 1

---
+++ final_work/1store/index.js
@@ -0,0 +1,10 @@
+function mouseOver()
+{
+    document.getElementById('drop-down').style.display = "block";
+    document.getElementById('list2').style.backgroundColor = "#ffffff";
+}
+function mouseOut()
+{
+    document.getElementById('drop-down').style.display = "none";
+    document.getElementById('list2').style.backgroundColor = "#f9f9f9";
+}
